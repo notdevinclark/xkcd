2a91805fc19883a03e72543b5ee7f01512fe5260
Devin Clark <notdevinclark@gmail.com>
2016-03-05 18:50:12 -0600

    fleshes out the xkcd module with new functions
    with docs and tests

--- lib/xkcd.ex
+++ lib/xkcd.ex
@@ -1,2 +1,71 @@
 defmodule Xkcd do
+  @moduledoc """
+  A client for the XKCD JSON API
+  """
+
+  alias Xkcd.Comic
+  alias HTTPoison.Response
+
+  @doc """
+  Takes a number from 1 to the latest and returns a tuple with with :ok and a
+  comic struct or an :error atom and a error message
+
+  Example:
+
+      iex> Xkcd.number(256)
+      {:ok, %Xkcd.Comic{...}}
+
+      iex> Xkcd.number(0)
+      {:error, "Comic Not Found"}
+  """
+  @spec number(integer) :: {:ok, Comic.t}
+                         | {:error, String.t}
+  def number(integer) do
+    "http://xkcd.com/#{integer}/info.0.json"
+    |> get_comic
+  end
+
+
+  @doc """
+  Gets the latest comic and returns a tuple with :ok and a comic struct
+
+  Example:
+
+      iex> Xkcd.latest
+      {:ok, %Xkcd.Comic{...}}
+  """
+  @spec latest :: {:ok, Comic.t}
+  def latest do
+    "http://xkcd.com/info.0.json"
+    |> get_comic
+  end
+
+  @doc """
+  Gets a random comic and returns a tuple with :ok and a comic struct
+
+  Example:
+
+      iex> Xkcd.random
+      {:ok, %Xkcd.Comic{...}}
+  """
+  @spec random :: {:ok, Comic.t}
+  def random do
+    {:ok, %Comic{num: max_num}} = Xkcd.latest
+    max_num
+    |> :rand.uniform
+    |> number
+  end
+
+  defp get_comic(url) do
+    url
+    |> HTTPoison.get
+    |> get_body
+    |> parse_body
+  end
+
+  defp get_body({:ok, %Response{status_code: 404}}), do: {:error, "Comic Not Found"}
+  defp get_body({:ok, %Response{body: body}}), do: {:ok, body}
+
+  defp parse_body({:ok, body}), do: {:ok, Poison.decode!(body, as: %Comic{})}
+  defp parse_body(error), do: error
 end
--- test/xkcd_test.exs
+++ test/xkcd_test.exs
@@ -1,8 +1,24 @@
 defmodule XkcdTest do
-  use ExUnit.Case
-  doctest Xkcd
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Xkcd.Comic
+
+  test "number gets a specific comic by it's number" do
+    retrieved_comic = %Comic{title: "Those Not Present", alt: "'Yeah, that squid's a total asshole.' [scoot scoot]", img: "http://imgs.xkcd.com/comics/those_not_present.png", num: 1176}
+    assert {:ok, ^retrieved_comic} = Xkcd.number(1176)
+  end
+
+  test "number returns an error if the comic doesn't exist" do
+    assert {:error, "Comic Not Found"} = Xkcd.number(0)
+  end
+
+  test "latest returns the latest comic" do
+    assert {:ok, %Comic{}} = Xkcd.latest
+  end
+
+  test "random gets a random comic" do
+    {:ok, %Comic{num: max_num}} = Xkcd.latest
+    {:ok, %Comic{num: num}} = Xkcd.random
+    assert num in 1..max_num
   end
 end
